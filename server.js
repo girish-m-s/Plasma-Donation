540eef8d996a2c6c6064b0de8b347f53fd90e83c
Girish M Saraswathipura <67829758+girish-m-s@users.noreply.github.com>
2020-10-31 17:23:59 +0530

    Database Implementation

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 
 const app = express();
-app.use(bodyParser.urlencoded({extended : true}));
 
+app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/patientDB", {useNewUrlParser : true});
+
+const hospitalSchema = new mongoose.Schema ({
+    hospitalName : String,
+    Address : {
+        City : String,
+        Pincode : Number,
+        Addressline : String 
+    },
+    IRDIANumber : Number,
+    ContactNumber : Number,
+    password : String,
+    email : String
+});
+const HospitalData = mongoose.model("HospitalData", hospitalSchema);
 
 app.get("/", function(req , res){
     res.sendFile(__dirname + "/public/page1.html");
@@ -13,8 +29,21 @@ app.get("/", function(req , res){
 
 app.post("/", function(req, res){
     console.log(req.body);
+    const hospitalData = new HospitalData ({
+        hospitalName : "Jayadeva Hospital",
+        Address : {
+            City : "Bangalore",
+            Pincode : 560072,
+            Addressline : "agsjasuha ahsahsua"
+        },
+        IRDIANumber : 234344,
+        ContactNumber : 9838288822,
+        password : "guhushahs"
+    });
+    hospitalData.save();
 });
 
 app.listen(3000, function(){
     console.log("hello");
 })
+
